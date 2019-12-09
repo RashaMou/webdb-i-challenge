2c52740aaee0509a7d259a4e6b699084a702b025
Rasha Moumneh <rasha@fastmail.com>
2019-12-09 17:23:26 -0500

    Completed MVP

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -29,4 +29,57 @@ router.get("/:id", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  const accountData = req.body;
+  knex("accounts")
+    .insert(accountData, "id")
+    .then(ids => {
+      const id = ids[0];
+      knex("*")
+        .from("accounts")
+        .where({ id })
+        .first()
+        .then(account => {
+          res.status(201).json(account);
+        })
+        .catch(error => {
+          res.status(500).json("Error retrieving account with specified id");
+        });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json("Error adding account");
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  knex("accounts")
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      res.status(200).json({ message: `${count} record(s) deleted` });
+    })
+    .catch(error => {
+      res.status(500).json({ message: "error deleting the account" });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const accountUpdate = req.body;
+
+  knex("accounts")
+    .where({ id: req.params.id })
+    .update(accountUpdate)
+    .then(count => {
+      if (count > 0) {
+        res.status(201).json({ message: `${count} record(s) updated` });
+      } else {
+        res.status(404).json("Record not found");
+      }
+    })
+    .catch(error => {
+      res.status(500).json("Error updating record");
+    });
+});
+
 module.exports = router;
